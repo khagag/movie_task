4f93c7c83e0d009376a19e27c444dbaf389a8be5
khagag <the_lord29@yahoo.com>
2020-11-12 11:05:58 +0200

    [create] create form view

---
+++ resources/views/create.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header"><b>{{ ucfirst($title) }} Create</b></div>
+                <div class="card-body">
+                  <form class="" action={{Route($title.'.store')}} method="post">
+                    @csrf
+                    <div class="form-group">
+                      <label for="Name">Name</label>
+                      <input type="text" required class="form-control" id="Name" placeholder="Enter Movie Name">
+                    </div>
+                    <div class="form-group">
+                      <label for="description">Description</label>
+                      <textarea class="form-control" required id="description" placeholder="Enter The Description"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                  </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
