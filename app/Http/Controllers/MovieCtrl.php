3824b87b06ca13bb3a2aff5537a217c2cacacf59
khagag <the_lord29@yahoo.com>
2020-11-12 13:50:57 +0200

    [modify] restrict access to movies & shows routes using auth middleware

--- app/Http/Controllers/MovieCtrl.php
+++ app/Http/Controllers/MovieCtrl.php
@@ -7,6 +7,12 @@ use App\Models\Movie;
 
 class MovieCtrl extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
--- app/Http/Controllers/ShowCtrl.php
+++ app/Http/Controllers/ShowCtrl.php
@@ -7,6 +7,12 @@ use App\Models\Show;
 
 class ShowCtrl extends Controller
 {
+
+  public function __construct()
+  {
+      $this->middleware('auth');
+  }
+
     /**
      * Display a listing of the resource.
      *
