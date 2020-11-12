01a663be94acbd9daf5178e64f017e369a4d9a80
khagag <the_lord29@yahoo.com>
2020-11-12 10:36:10 +0200

    [add] name & description to shows & movies

--- database/migrations/2020_11_12_015812_create_shows_table.php
+++ database/migrations/2020_11_12_015812_create_shows_table.php
@@ -15,6 +15,8 @@ class CreateShowsTable extends Migration
     {
         Schema::create('shows', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
             $table->timestamps();
         });
     }
