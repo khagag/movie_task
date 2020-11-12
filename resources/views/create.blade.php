1114b2fe18a5889e6d40ea617fdf64480fac890e
khagag <the_lord29@yahoo.com>
2020-11-12 11:51:09 +0200

    [add] name attribute and flash msg to create view

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -4,6 +4,9 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12">
+          @if(session()->exists('status'))
+            <div class="alert alert-danger" role='alert'>{{session()->get('status')}}</div>
+          @endif
             <div class="card">
                 <div class="card-header"><b>{{ ucfirst($title) }} Create</b></div>
                 <div class="card-body">
@@ -11,11 +14,11 @@
                     @csrf
                     <div class="form-group">
                       <label for="Name">Name</label>
-                      <input type="text" required class="form-control" id="Name" placeholder="Enter Movie Name">
+                      <input type="text" required class="form-control" id="Name" name="name" placeholder="Enter Movie Name">
                     </div>
                     <div class="form-group">
                       <label for="description">Description</label>
-                      <textarea class="form-control" required id="description" placeholder="Enter The Description"></textarea>
+                      <textarea class="form-control" required id="description" name="description" placeholder="Enter The Description"></textarea>
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                   </form>
